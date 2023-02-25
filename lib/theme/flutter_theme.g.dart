2026a9a006486cde78e8bfd03092d68a37ea2684
Parabeac-Nest <admin@parabeac.com>
2023-02-25 05:43:32 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ lib/theme/flutter_theme.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';import 'package:flutter/theme/flutter_text_styles.g.dart';
+import 'package:flutter/theme/flutter_colors.g.dart';
+
+class FlutterTheme {
+
+static const TextTheme textTheme = TextTheme(headline1: FlutterTextStyles.headline1,headline2: FlutterTextStyles.headline2,headline3: FlutterTextStyles.headline3,headline4: FlutterTextStyles.headline4,headline5: FlutterTextStyles.headline5,headline6: FlutterTextStyles.headline6,);
+
+static const ColorScheme light = ColorScheme.light(onPrimary: FlutterColors.lightOnPrimary,primary: FlutterColors.lightPrimary,primaryContainer: FlutterColors.lightPrimaryContainer,secondary: FlutterColors.lightSecondary,onSecondary: FlutterColors.lightOnSecondary,onPrimaryContainer: FlutterColors.lightOnPrimaryContainer,secondaryContainer: FlutterColors.lightSecondaryContainer,onSecondaryContainer: FlutterColors.lightOnSecondaryContainer,shadow: FlutterColors.lightShadow);
+
+static  ThemeData themeDataLight = ThemeData(textTheme: textTheme, colorScheme: light,);
+
+static const ColorScheme dark = ColorScheme.dark(primary: FlutterColors.darkPrimary,onPrimary: FlutterColors.darkOnPrimary,primaryContainer: FlutterColors.darkPrimaryContainer,onPrimaryContainer: FlutterColors.darkOnPrimaryContainer,secondary: FlutterColors.darkSecondary,onSecondary: FlutterColors.darkOnSecondary,secondaryContainer: FlutterColors.darkSecondaryContainer,onSecondaryContainer: FlutterColors.darkOnSecondaryContainer,shadow: FlutterColors.darkShadow);
+
+static  ThemeData themeDataDark = ThemeData(textTheme: textTheme, colorScheme: dark,);
+}
